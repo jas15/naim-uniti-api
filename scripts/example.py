e8f14cc8516a5ac6c8448cd0aa40650f15d06ce1
Jordan Stewart <jstewart@palantir.com>
2021-01-16 11:10:41 +0000

    Add skeleton file structure, setup.py

---
+++ scripts/example.py
@@ -0,0 +1,6 @@
+# TODO this script should create a new Uniti station given an IP arg and
+# optional --test arg, then call a few as-yet-undefined things.
+
+print("TODO")
+exit(1)
+
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+
+setup(
+    name='NaimUnitiAPI',
+    version='0.1.0',
+    author='Jordan Stewart',
+    packages=['naim_uniti_api', 'naim_uniti_api.test'],
+    scripts=['scripts/example.py'],
+    license='LICENSE.txt',
+    description='A for-personal-use API to control Naim Uniti devices.',
+    long_description=open('README.txt').read(),
+    install_requires=[ ],
+)
